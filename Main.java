407bd3287403b2af7fb2c9bba6851bb0815fea7c
Meilianda-anr <meiliandanrahman@gmail.com>
2025-07-06 18:30:26 +0800

    Import Library

--- Main.java
+++ Main.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package pertemuan_terakhir;
 
-/**
- *
- * @author USER
- */
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
     
 }
