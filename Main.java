1604647d5cf995bd96a85a6499081ecc810cba0f
Meilianda-anr <meiliandanrahman@gmail.com>
2025-07-06 18:49:01 +0800

    perulangan dan penyimpanan

--- Main.java
+++ Main.java
@@ -16,5 +16,26 @@ public class Main {
         System.out.print("Jumlah Sepatu yang Ingin Dicuci: ");
         int jumlah = sc.nextInt();
         
+        //perulangan dan penyimpanan
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("\n--- Sepatu ke-" + (i+i) + "---");
+            System.out.print("Nama Sepatu: ");
+            String namaSepatu = sc.nextLine();
+            
+            System.out.print("Ukuran Sepatu: ");
+            int ukuran = sc.nextInt();
+            sc.nextLine();
+            
+            System.out.println("Pilih Jenis Pembersihan: ");
+            System.out.println("1. Reguler Cleaning  = Rp 20.000 (5 jam - 1 hari)");
+            System.out.println("2. Fast Cleaning     = Rp 40.000 (1 jam)");
+            System.out.println("3. Deep Cleaning     = Rp 60.000 - 100.000 (Perawatan menyeluruh");
+            System.out.print("Pilihan (1/2/3): ");
+            int pilihan = sc.nextInt();
+            sc.nextLine();
+            
+            String layanan = "";
+            double harga = 0;
+        }
     }
 }
