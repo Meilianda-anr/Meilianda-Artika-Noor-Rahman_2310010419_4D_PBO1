6f1df65f1a3fbd201da7cbd924143d1a5d8a0b6c
Meilianda-anr <meiliandanrahman@gmail.com>
2025-07-06 18:37:49 +0800

    bagian import data nama dan jumlah

--- Main.java
+++ Main.java
@@ -6,5 +6,15 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Main {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        List<Sepatu> daftarSepatu = new ArrayList<>();
+        
+        System.out.print("Masukkan Nama Pelanggan: ");
+        String nama = sc.nextLine();
+        
+        System.out.print("Jumlah Sepatu yang Ingin Dicuci: ");
+        int jumlah = sc.nextInt();
+        
+    }
 }
