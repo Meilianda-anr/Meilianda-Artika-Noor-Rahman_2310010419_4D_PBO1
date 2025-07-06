30f3b3a51cb8498c903fd36b0d2e8ccb4e5eaf90
Meilianda-anr <meiliandanrahman@gmail.com>
2025-07-06 18:20:37 +0800

    selesai membuat class sepatu

--- Sepatu.java
+++ Sepatu.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package pertemuan_terakhir;
 
-/**
- *
- * @author USER
- */
+import java.util.*;
+
 public class Sepatu {
+    private String namaSepatu;
+    private int ukuran;
+    private String layanan;
+    private double harga;
     
+    public Sepatu(String namaSepatu, int ukuran, String layanan, double harga){
+        this.namaSepatu = namaSepatu;
+        this.ukuran = ukuran;
+        this.layanan = layanan;
+        this.harga = harga;
+    }
+    
+    public double getHarga(){
+        return harga;
+    }
+    
+    public void tampilkan(){
+        System.out.println("Nama Speatu: " + namaSepatu);
+        System.out.println("Ukuran: " + ukuran);
+        System.out.println("Layanan: " + layanan);
+        System.out.println("Harga: Rp " + (int) harga);
+        System.out.println("--------------------------");
+    }
 }
